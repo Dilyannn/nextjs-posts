63fe137f58f90134190c4d77807f2f76e8aef6e5
Dilyannn <122313669+Dilyannn@users.noreply.github.com>
2026-02-11 22:55:18 +0200

    use revalidatePath function to make a page retrieve the current data in the db

--- actions/like.js
+++ actions/like.js
@@ -1,7 +1,9 @@
 "use server";
 
 import { updatePostLikeStatus } from "@/lib/posts.js";
+import { revalidatePath } from "next/cache.js";
 
 export default async function likePostStatus(postId) {
   await updatePostLikeStatus(postId, 2);
+  revalidatePath("/feed");
 }
